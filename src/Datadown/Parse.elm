f1fdf880e6d0f4af510b362b2e11c6a30e1210f0
Patrick Smith <patrick@burntcaramel.com>
2018-01-08 23:40:59 +1100

    Remove unused code

--- src/Datadown/Parse.elm
+++ src/Datadown/Parse.elm
@@ -15,49 +15,9 @@ module Datadown.Parse
 
 import Datadown exposing (Document, Section)
 import Datadown.Content as Content exposing (Content(..))
-
-
--- import String
--- import Regex exposing (Regex)
-
 import Dict
 import Markdown.Block as Block exposing (Block(..))
 import Markdown.Inline as Inline exposing (Inline(..))
-
-
--- tagsRegex : Regex
--- tagsRegex = Regex.regex "\\B#[A-Za-z0-9_-]+(:\\s*[^#]*)?"
--- tagKeyValueRegex : Regex
--- tagKeyValueRegex = Regex.regex "\\B#([a-zA-Z0-9-_]+)(:\\s*([^#]*))?"
--- mentionsRegex : Regex
--- mentionsRegex = Regex.regex "@([a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9-_]+)*)"
--- introductionsRegex : Regex
--- introductionsRegex = Regex.regex "^@([a-zA-Z0-9_-]+):[\\s]*(.*)"
--- reduceMultipleSpaces : String -> String
--- reduceMultipleSpaces = Regex.replace Regex.All (Regex.regex "\\s+") (\_ -> " ")
--- nonEmptyRegex : Regex
--- nonEmptyRegex = Regex.regex "\\S"
--- rejectEmptyStrings : List String -> List String
--- rejectEmptyStrings = List.filter (Regex.contains nonEmptyRegex)
--- parseIntroduction : String -> { introduction: Maybe String, rest: String }
--- parseIntroduction input =
---   input
---   |> Regex.find Regex.All introductionsRegex
---   |> \matches ->
---     case matches of
---       match::_ ->
---         case match.submatches of
---           introduction::rest::[] ->
---             case rest of
---               Just rest ->
---                 { introduction = introduction, rest = rest }
---               Nothing ->
---                 { introduction = introduction, rest = "" }
---           _ ->
---             { introduction = Nothing, rest = input }
---       _ ->
---         { introduction = Nothing, rest = input }
-
 
 {-| Parses a Datadown section
 
